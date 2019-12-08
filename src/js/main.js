b4bbc999dde6486f1caba74adad9842776d6d11e
Nikolai Kononenko <nikolay.kononenko@gmail.com>
2019-12-08 21:57:55 +0200

    add mobile menu and ingredients list

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
-var overlay = document.getElementsByClassName('hamburger-menu');
-var btn = document.getElementsByClassName('hamburger-menu__btn');
-console.dir (btn);
+var overlay = document.querySelector('.hamburger-menu');
+var btn = document.querySelector('.hamburger-menu__btn');
+var body = document.querySelector ('body');
+
 
 function toggleMenu () {
     overlay.classList.toggle('hamburger-menu--active');
+    if (overlay.classList.contains('hamburger-menu--active')){
+        body.style.overflow = 'hidden';
+    } else {
+        body.style.overflow = 'visible';
+    }
 }
 btn.addEventListener('click', toggleMenu);
